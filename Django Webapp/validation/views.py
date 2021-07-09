4aeac28d5d8e0b3f1a1a430b7b5f391974dcc946
Roman <roman.shrestha312@gmail.com>
2021-07-09 12:22:23 +0545

    added missing files

---
+++ Django Webapp/validation/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+
+import requests
+import time
+
+message_obj_list = []
+link=r""http://offensivedetectormodel-env.eba-saifiyvj.us-east-1.elasticbeanstalk.com/predict
+
+def home(request):
+    if request.method == "POST":
+        name = request.POST["name"]
+        email = request.POST["email"]
+        message = request.POST["message"]
+        r=requests.get(f"{link}/{message}")
+        offensive=r.json()["isOffensive"]
+        if not offensive:
+            message_obj = {"name": name, "email": email, "message": message}
+            message_obj_list.append(message_obj)
+        else:
+            messages.error(
+                request, f"Your post was detected as offensive and has been removed by our system")
+            redirect("/")
+    return render(request, 'index.html', context={"posts": message_obj_list})
